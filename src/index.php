4ef49f3b4454d3fcb943dc69e8ca8fcd0e7bec4a
root <grzegorzchrymecki@php-developer.ie>
2021-07-04 17:40:05 +0100

    ok

--- src/index.php
+++ src/index.php
@@ -1,7 +1,17 @@
 <?php
 
+require __DIR__ .'./../vendor/autoload.php';
+
+use App\Entity\Entity;
+use App\Entity\User;
+
 $request = json_encode($_REQUEST, true);
 
 //file_put_contents('req.txt', print_r(json_decode($_REQUEST['object'], true),true));
 
-echo json_encode($_REQUEST);
+$user= new User();
+$entity = new Entity($user);
+$entity->read(1);
+
+
+echo json_encode($user->getUsername());
